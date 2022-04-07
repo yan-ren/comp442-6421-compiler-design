521487096f429590c11d561fcfbefa9175e6d797
Yan Ren <ryan.ren3@gmail.com>
2022-04-07 10:44:59 -0400

    object basic working

--- output/code_gen/object/object.m
+++ output/code_gen/object/object.m
@@ -16,19 +16,21 @@
                                             % begin of function definition: evaluate
  evaluate                                   
             sw         -4(r14),r15          % push r15 on stack frame
-            addi       r1,r0,0              % load intnum: 0
+            addi       r1,r0,2              % load intnum: 2
             sw         -16(r14),r1          
             lw         r1,-16(r14)          % load value for result
             sw         -12(r14),r1          % assign result
-            lw         r1,0(r14)            % mulOp operation
+            addi       r1,r0,1              % load intnum: 1
+            sw         -20(r14),r1          
+            lw         r1,-12(r14)          % mulOp operation
             lw         r2,-8(r14)           
             mul        r3,r1,r2             
-            sw         -20(r14),r3          
-            lw         r1,-4(r14)           % addOp operation
-            lw         r2,-20(r14)          
-            add        r3,r1,r2             
             sw         -24(r14),r3          
-            lw         r1,-24(r14)          % load value for result
+            lw         r1,-20(r14)          % addOp operation
+            lw         r2,-24(r14)          
+            add        r3,r1,r2             
+            sw         -28(r14),r3          
+            lw         r1,-28(r14)          % load value for result
             sw         -12(r14),r1          % assign result
             lw         r1,-12(r14)          % print var: result
             jl         r15,putint           
@@ -83,13 +85,23 @@
             addi       r1,r1,1              
             lb         r2,newline(r1)       
             putc       r2                   
+            addi       r1,r0,3              % load intnum: 3
+            sw         -20(r14),r1          
+                                            % function call to evaluate
+            lw         r1,-20(r14)          % pass t8_2101153819 into parameter
+            sw         -36(r14),r1          
+            subi       r14,r14,28           % increment stack frame
+            jl         r15,evaluate         % jump to funciton: evaluate
+            addi       r14,r14,28           % decrement stack frame
+            lw         r1,-28(r14)          % get return value from evaluate
+            sw         -24(r14),r1          
             addi       r1,r0,0              % load l1
             lw         r2,l1(r1)            
             addi       r3,r0,4              % load value for l1
             lw         r4,l1(r3)            
             add        r5,r2,r4             
-            sw         -20(r14),r5          
-            lw         r1,-20(r14)          % print var: t7_2101153819
+            sw         -28(r14),r5          
+            lw         r1,-28(r14)          % print var: t10_2101153819
             jl         r15,putint           
             addi       r1,r0,0              % print a newline
             lb         r2,newline(r1)       
